a28089c6f47742c278bf621bf56b5d0e0c3a2e73
LindaJansone <lindajansone04@gmail.com>
2024-06-27 23:17:00 +0300

    CSS line problems fixed

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -125,22 +125,25 @@
                     <p><b>@lang('msg.price'):</b> <em>{{ $post->price }}</em>@lang('msg.hour')</p>
                     <p><b>@lang('msg.language'): </b><em>@if($post->language === 'Other') @lang('msg.other') @else{{ $post->language }} @endif</em></p>
 
-                    @can('delete-post', $post)
-                    <p>
-                        <form method="POST" action="{{ route('posts.destroy', $post->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">@lang('msg.delete_post')</button>
-                        </form>
-                    </p>
-                    @endcan
-                    @can('update-post', $post)
-                    <p>
-                        <form method="GET" action="{{ route('posts.edit', $post->id) }}">
-                            <button type="submit">@lang('msg.edit_post')</button>
-                        </form>
-                    </p>
-                    @endcan
+                    <div class="button-container">
+                        @can('delete-post', $post)
+                        <p>
+                            <form method="POST" action="{{ route('posts.destroy', $post->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">@lang('msg.delete_post')</button>
+                            </form>
+                        </p>
+                        @endcan
+                        
+                        @can('update-post', $post)
+                        <p>
+                            <form method="GET" action="{{ route('posts.edit', $post->id) }}">
+                                <button type="submit">@lang('msg.edit_post')</button>
+                            </form>
+                        </p>
+                        @endcan
+                    </div>
                 </div>
             @endforeach
         </div>
